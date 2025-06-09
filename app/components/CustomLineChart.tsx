d28c4b1f2edf8fb6c749a652dc4abcfbd6991c39
Anton <anton.e.schaefer@icloud.com>
2025-06-09 19:36:39 +0200

    first graph

---
+++ app/components/CustomLineChart.tsx
@@ -0,0 +1,103 @@
+"use client";
+
+import React, { useEffect } from "react";
+import { useState } from "react";
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+} from "recharts";
+
+interface MonthlyData {
+  calendarDate: string;
+  numberOfHOBs: string;
+  numberOfBloodCultureSamples: string;
+  numberOfPatientDays: string;
+  bcRate: string;
+  hobRate: string;
+}
+
+interface RawData {
+  hobType: string;
+  periodType: string;
+  calendarDateStart: string;
+  calendarDateEnd: string;
+  wardGroupType: string;
+  wardGroupValue: string;
+  numberOfHOBs: string;
+  numberOfBloodCultureSamples: string;
+  numberOfPatientDays: string;
+  bcRate: string;
+  hobRate: string;
+}
+
+interface Props {
+  startDate?: string;
+  endDate?: string;
+  hobType?: string;
+  wardGroupType?: string;
+  wardGroupValue?: string;
+}
+
+const CustomLineChart = ({
+  startDate,
+  endDate,
+  hobType,
+  wardGroupType,
+  wardGroupValue,
+}: Props) => {
+  const [chartData, setChartData] = useState<MonthlyData[]>([]);
+  useEffect(() => {
+    const loadData = async () => {
+      try {
+        let queryParams =
+          "?periodType=MONTHLY&hobType=ALL&wardGroupType=HOSPITAL";
+        queryParams += `&calendarDateStart=${startDate}`;
+        queryParams += `&calendarDateEnd=${endDate}`;
+        const res = await fetch(
+          "http://localhost:3000/api/hob-rates" + queryParams
+        );
+        if (!res.ok) throw new Error(`HTTP Fehler! Status ${res.status}`);
+        const rows: RawData[] = await res.json();
+
+        setChartData(
+          rows.map((raw) => ({
+            calendarDate: raw.calendarDateStart.slice(3),
+            numberOfHOBs: raw.numberOfHOBs,
+            numberOfBloodCultureSamples: raw.numberOfBloodCultureSamples,
+            numberOfPatientDays: raw.numberOfPatientDays,
+            bcRate: raw.bcRate,
+            hobRate: raw.hobRate,
+          }))
+        );
+      } catch (err) {
+        console.log("Fehler beim Lesen der Daten:", err);
+      }
+    };
+    loadData();
+  }, []);
+
+  return (
+    <>
+      <div>CustomLineChart, {startDate}</div>
+      <ResponsiveContainer width="100%" height={300}>
+        <LineChart data={chartData}>
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="calendarDate" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          {<Line type="monotone" dataKey="numberOfHOBs" stroke="#8884d8" />}
+          {/* {showB && <Line type="monotone" dataKey="valueB" stroke="#82ca9d" />} */}
+        </LineChart>
+      </ResponsiveContainer>
+    </>
+  );
+};
+
+export default CustomLineChart;
